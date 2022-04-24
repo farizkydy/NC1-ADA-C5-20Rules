77acb392204320a568fd50efccc8d4385c856570
Fariz Rizky Rizaldy <farizrizkyrizaldy@gmail.com>
2022-04-24 20:05:56 +0700

    Initial Commit

---
+++ TestNC1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestNC1
+//
+//  Created by Fariz Rizky Rizaldy on 24/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
